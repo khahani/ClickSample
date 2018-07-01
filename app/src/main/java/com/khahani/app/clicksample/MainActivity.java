e5136d4d82703ff37e5e0f0e41a806dd4771459f
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-07-01 07:35:33 -0700

    استفاده از فایندویوبای آیدی

--- app/src/main/java/com/khahani/app/clicksample/MainActivity.java
+++ app/src/main/java/com/khahani/app/clicksample/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,13 +13,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.use_findviewbyid_sample);
+
+        EditText nameEditText = (EditText)findViewById(R.id.name);
+        nameEditText.setText("lotfan name ra vared konid");
     }
 
-    public void do_my_work(View view){
-        int i = 10;
-        Log.e("TAG", "This is my code");
-        Toast.makeText(this, "Salam", Toast.LENGTH_LONG).show();
+    public void salamKon(View view){
+
+        //1 - Karbar name ra vared mikonid
+        //2 - Karbar roye button click mikonad
+        //3 - Farakhani view az layout
+        EditText nameEditText  = (EditText)findViewById(R.id.name);
+        //4 - daryaft maghadire morede niyaz
+        String name = nameEditText.getText().toString();
+        //5 - Farakhani view az layout
+        TextView message = (TextView) findViewById(R.id.message);
+        //6 - Meghdardehi be layout
+        message.setText("Hello mr/mrs " + name);
+
     }
+
 
 }
