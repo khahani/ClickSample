aa840b603d438bae27d52eef1d0bd432d51ad03f
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-07-01 08:39:13 -0700

    روش های گوناگون تعریف رویداد کلیک

--- app/src/main/java/com/khahani/app/clicksample/MainActivity.java
+++ app/src/main/java/com/khahani/app/clicksample/MainActivity.java
@@ -4,29 +4,83 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements View.OnClickListener{
+
+    private View.OnClickListener ClickHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.e("TAg", "Ravesh 4om");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.use_findviewbyid_sample);
 
-        EditText nameEditText = (EditText)findViewById(R.id.name);
+        EditText nameEditText = (EditText) findViewById(R.id.name);
         nameEditText.setText("lotfan name ra vared konid");
 
+        Button clickMe = (Button) findViewById(R.id.clickme);
+        Button sayHello = (Button) findViewById(R.id.sayHello);
+
+        clickMe.setOnClickListener(this);
+        sayHello.setOnClickListener(this);
+
+        sayHello.setOnClickListener(ClickHandler);
+
+       /* //1 Farakhani view az layout
+        Button clickMe = (Button) findViewById(R.id.clickme);
+        clickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button sayHello = (Button)findViewById(R.id.sayHello);
+        sayHello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //1 - Karbar name ra vared mikonid
+                //2 - Karbar roye button click mikonad
+                //3 - Farakhani view az layout
+                EditText nameEditText = (EditText) findViewById(R.id.name);
+                //4 - daryaft maghadire morede niyaz
+                String name = nameEditText.getText().toString();
+
+                // 1-1 - Farakhani view az layout
+                CheckBox isFemaleChekBox = (CheckBox) findViewById(R.id.isFemale);
+                // 1-2 - daryaft maghadir morede niyaz
+                boolean isFemale = isFemaleChekBox.isChecked();
+                // 1-3
+                String mrOrMrs;
+                if (isFemale) {
+                    mrOrMrs = "Mrs";
+                } else {
+                    mrOrMrs = "Mr";
+                }
+                //5 - Farakhani view az layout
+                TextView message = (TextView) findViewById(R.id.message);
+                //6 - Meghdardehi be layout
+                message.setText("Hello " + mrOrMrs + " " + name);
+            }
+        });*/
     }
 
-    public void salamKon(View view){
+    public void salamKon(View view) {
 
         //1 - Karbar name ra vared mikonid
         //2 - Karbar roye button click mikonad
         //3 - Farakhani view az layout
-        EditText nameEditText  = (EditText)findViewById(R.id.name);
+        EditText nameEditText = (EditText) findViewById(R.id.name);
         //4 - daryaft maghadire morede niyaz
         String name = nameEditText.getText().toString();
 
@@ -36,9 +90,9 @@ public class MainActivity extends AppCompatActivity {
         boolean isFemale = isFemaleChekBox.isChecked();
         // 1-3
         String mrOrMrs;
-        if (isFemale){
+        if (isFemale) {
             mrOrMrs = "Mrs";
-        }else{
+        } else {
             mrOrMrs = "Mr";
         }
         //5 - Farakhani view az layout
@@ -49,4 +103,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View view) {
+
+        int id = view.getId();
+
+        switch (id){
+            case R.id.sayHello:{
+                //1 - Karbar name ra vared mikonid
+                //2 - Karbar roye button click mikonad
+                //3 - Farakhani view az layout
+                EditText nameEditText = (EditText) findViewById(R.id.name);
+                //4 - daryaft maghadire morede niyaz
+                String name = nameEditText.getText().toString();
+
+                // 1-1 - Farakhani view az layout
+                CheckBox isFemaleChekBox = (CheckBox) findViewById(R.id.isFemale);
+                // 1-2 - daryaft maghadir morede niyaz
+                boolean isFemale = isFemaleChekBox.isChecked();
+                // 1-3
+                String mrOrMrs;
+                if (isFemale) {
+                    mrOrMrs = "Mrs";
+                } else {
+                    mrOrMrs = "Mr";
+                }
+                //5 - Farakhani view az layout
+                TextView message = (TextView) findViewById(R.id.message);
+                //6 - Meghdardehi be layout
+                message.setText("Hello " + mrOrMrs + " " + name);
+                break;
+            }
+            case R.id.clickme: {
+                Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
+
+                break;
+            }
+        }
+
+    }
 }
